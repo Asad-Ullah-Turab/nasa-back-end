d381f5880ec178e03f5763aede10744c3bad0ef4
asad-ullah-turab <asadullahturab47@gmail.com>
2025-07-16 16:34:33 +0500

    add catch all path for frontend client side routing

--- src/app.js
+++ src/app.js
@@ -14,16 +14,16 @@ app.use(
   }),
 );
 
-app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(morgan("combined"));
 
 // Routers
 app.use("/planets", planetsRouter);
 app.use("/launches", launchesRouter);
 
 // Serve frontend
-app.get("/", (req, res) => {
+app.get("/*splat", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
