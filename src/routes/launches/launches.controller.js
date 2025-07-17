c8f1662d229b4320e01cf87b0fd2d206967a587a
asad-ullah-turab <asadullahturab47@gmail.com>
2025-07-17 18:39:24 +0500

    Add error validation in submitting launch

--- src/routes/launches/launches.controller.js
+++ src/routes/launches/launches.controller.js
@@ -6,9 +6,25 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
-  launch.launchDate = new Date(launch.launchDate);
-  const result = addNewLaunch(launch);
 
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.target
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property",
+    });
+  }
+  launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Invalid launch date",
+    });
+  }
+
+  const result = addNewLaunch(launch);
   return res.status(201).json(result);
 }
 
