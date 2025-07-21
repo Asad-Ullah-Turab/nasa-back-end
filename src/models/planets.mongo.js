e699c16f0e4d6856659cc251f670e20a5665a254
asad-ullah-turab <asadullahturab47@gmail.com>
2025-07-21 15:07:13 +0500

    Add: launches and planets schemas and models

---
+++ src/models/planets.mongo.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const planetsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Planet", planetsSchema);
