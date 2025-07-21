f404f7b9f205357b3a61202ff4de8c4fd864fecf
asad-ullah-turab <asadullahturab47@gmail.com>
2025-07-21 14:24:27 +0500

    Add: connection to mongodb atlas

--- src/server.js
+++ src/server.js
@@ -1,17 +1,30 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 const { loadHabitablePlanets } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
-
 const server = http.createServer(app);
 
+const MONGO_URL = process.env.MONGO_URL;
+
+if (!MONGO_URL) {
+  throw new Error("MONGO_URL environment variable is not set");
+}
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection established successfully.");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL, {});
   await loadHabitablePlanets();
   server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}..`);
   });
 }
-
 startServer();
